c19a5438800bbc34779a8d88e88e499390ffaad3
Likhith karri <99127911+likhi-23@users.noreply.github.com>
2022-03-08 22:50:06 +0530

    Delete Q1-7.py

--- task-8/Q1-7.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-given_vector = np.array([10, 11, 12, 13, 14])
-print("Original array  ")
-print(given_vector)
-a = 5
-new_vector = np.zeros(len(given_vector) + (len(given_vector)-1)*(a))
-new_vector[::a+1] = given_vector
-print("\n New array ")
-print(new_vector)
